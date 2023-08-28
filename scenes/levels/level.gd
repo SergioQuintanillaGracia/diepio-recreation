7544245c2a27bbc01b11d975551f9aad766ae248
SergioQuintanillaGracia <sergioquintanillagracia@gmail.com>
2023-08-29 00:29:10 +0200

    Test commit

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -3,6 +3,7 @@ extends Node2D
 var bullet_normal_scene: PackedScene = preload("res://scenes/bullets/bullet_normal.tscn")
 
 func _on_player_shoot_bullet(pos, speed):
+	# Create a bullet, give it a position and a speed, and add it to the Bullets node
 	var bullet = bullet_normal_scene.instantiate() as RigidBody2D
 	bullet.position = pos
 	bullet.linear_velocity = speed
